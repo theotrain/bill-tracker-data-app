151697deb5e5463a21c34a2355b87260dd673815
theotrain <owen.is.here@gmail.com>
2025-01-01 18:27:18 -0800

    add api4

--- app/api/update/route.js
+++ app/api/update/route.js
@@ -1,8 +1,18 @@
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 import { getSheetData, updateDate, connect } from "@/helper/process-sheet";
+import { waitUntil } from "@vercel/functions";
+
+// function doSomething() {
+//   let promise = connect();
+//   let result = await promise;
+//   return result;
+// }
 
 export function GET(request) {
-  fetch("https://bill-tracker-data-app.vercel.app/update");
-  let r = connect();
-  return new Response(`Hello from api/update: ${r}`);
+  // waitUntil(connect()));
+  waitUntil(fetch("https://bill-tracker-data-app.vercel.app/update"));
+  return new Response(`Hello from owen`);
+  // doSomething().then((answer) => {
+  //   return new Response(`Hello from api/update: ${answer}`);
+  // });
 }
