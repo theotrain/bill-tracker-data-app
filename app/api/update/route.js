87bcbef86f5ef08a7183946732a80b0d1c7e3bc7
theotrain <owen.is.here@gmail.com>
2025-01-01 19:00:52 -0800

    lose api

--- app/api/update/route.js
+++ app/api/update/route.js
@@ -1,19 +1,8 @@
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
-import { getSheetData, updateDate, connect } from "@/helper/process-sheet";
-import { waitUntil } from "@vercel/functions";
-export const runtime = "edge";
+// export const dynamic = "force-dynamic";
+// import { waitUntil } from "@vercel/functions";
+// export const runtime = "edge";
 
-// function doSomething() {
-//   let promise = connect();
-//   let result = await promise;
-//   return result;
+// export function GET() {
+//   waitUntil(fetch("https://bill-tracker-data-app.vercel.app/update"));
+//   return new Response(`Hello from owen`);
 // }
-
-export function GET() {
-  // waitUntil(connect()));
-  waitUntil(fetch("https://bill-tracker-data-app.vercel.app/update"));
-  return new Response(`Hello from owen`);
-  // doSomething().then((answer) => {
-  //   return new Response(`Hello from api/update: ${answer}`);
-  // });
-}
